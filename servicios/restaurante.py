261593f023857a068a7b5ba5ef74c4bad9b63008
Erick Anchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-06-27 16:31:35 -0500

    Update restaurante.py

--- servicios/restaurante.py
+++ servicios/restaurante.py
@@ -1,1 +1,19 @@
+class Restaurante:
+    def __init__(self, nombre: str):
+        self.nombre = nombre
+        self.productos = []
+        self.clientes = []
 
+    def agregar_producto(self, producto):
+        self.productos.append(producto)
+
+    def agregar_cliente(self, cliente):
+        self.clientes.append(cliente)
+
+    def mostrar_productos(self):
+        for producto in self.productos:
+            print(producto)
+
+    def mostrar_clientes(self):
+        for cliente in self.clientes:
+            print(cliente)
