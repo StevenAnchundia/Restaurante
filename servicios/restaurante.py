f43e0ddedbca06c40e6e9052cbc7b63e3922d585
Erick Anchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-07-12 10:29:56 -0500

    Refactor Restaurante class methods and add client handling

--- servicios/restaurante.py
+++ servicios/restaurante.py
@@ -1,19 +1,56 @@
 from modelos.producto import Producto
+from modelos.cliente import Cliente
 
 
 class Restaurante:
-    """Administra los productos."""
 
     def __init__(self):
         self.productos: list[Producto] = []
+        self.clientes: list[Cliente] = []
 
-    def agregar_producto(self, producto: Producto):
+    # PRODUCTOS
+
+    def registrar_producto(self, producto: Producto):
         self.productos.append(producto)
 
-    def mostrar_productos(self):
+    def listar_productos(self):
 
-        print("===== PRODUCTOS =====\n")
+        if not self.productos:
+            print("No existen productos.")
+            return
 
         for producto in self.productos:
-            producto.mostrar_informacion()
-            print("----------------------")
+            print(producto.mostrar_informacion())
+
+    def buscar_producto(self, nombre: str):
+
+        for producto in self.productos:
+            if producto.nombre.lower() == nombre.lower():
+                print(producto.mostrar_informacion())
+                return
+
+        print("Producto no encontrado.")
+
+    # CLIENTES
+
+    def registrar_cliente(self, cliente: Cliente):
+        self.clientes.append(cliente)
+
+    def listar_clientes():
+
+        if not self.clientes:
+            print("No existen clientes.")
+            return
+
+        for cliente in self.clientes:
+            print(cliente)
+
+    def buscar_cliente(self, id_cliente):
+
+        for cliente in self.clientes:
+
+            if cliente.id_cliente == id_cliente:
+                print(cliente)
+                return
+
+        print("Cliente no encontrado.")
