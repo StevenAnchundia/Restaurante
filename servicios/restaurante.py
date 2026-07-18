b6e10e1bba811c58a756698a4698b81083c679fa
Erick Anchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-07-18 08:29:54 -0500

    Refactor Restaurante class methods and add type hints
    Refactor methods to improve readability and add type hints.

--- servicios/restaurante.py
+++ servicios/restaurante.py
@@ -1,56 +1,68 @@
-from modelos.producto import Producto
-from modelos.cliente import Cliente
-
+from modelos import Producto, Cliente
 
 class Restaurante:
+    """
+    Clase encargada de administrar productos y clientes.
+    """
 
     def __init__(self):
         self.productos: list[Producto] = []
         self.clientes: list[Cliente] = []
 
+    # ==========================
     # PRODUCTOS
+    # ==========================
 
-    def registrar_producto(self, producto: Producto):
+    def registrar_producto(self, producto: Producto) -> bool:
+
+        for producto_existente in self.productos:
+
+            if producto_existente.codigo == producto.codigo:
+                return False
+
         self.productos.append(producto)
+        return True
 
     def listar_productos(self):
 
-        if not self.productos:
-            print("No existen productos.")
+        if len(self.productos) == 0:
+            print("\nNo existen productos registrados.\n")
             return
 
+        print("\n========== PRODUCTOS ==========\n")
+
+        # Polimorfismo
         for producto in self.productos:
             print(producto.mostrar_informacion())
 
-    def buscar_producto(self, nombre: str):
+        print()
 
-        for producto in self.productos:
-            if producto.nombre.lower() == nombre.lower():
-                print(producto.mostrar_informacion())
-                return
-
-        print("Producto no encontrado.")
-
+    # ==========================
     # CLIENTES
+    # ==========================
 
-    def registrar_cliente(self, cliente: Cliente):
+    def registrar_cliente(self, cliente: Cliente) -> bool:
+
+        for cliente_existente in self.clientes:
+
+            if (
+                cliente_existente.identificacion
+                == cliente.identificacion
+            ):
+                return False
+
         self.clientes.append(cliente)
+        return True
 
-    def listar_clientes():
+    def listar_clientes(self):
 
-        if not self.clientes:
-            print("No existen clientes.")
+        if len(self.clientes) == 0:
+            print("\nNo existen clientes registrados.\n")
             return
 
-        for cliente in self.clientes:
-            print(cliente)
-
-    def buscar_cliente(self, id_cliente):
+        print("\n========== CLIENTES ==========\n")
 
         for cliente in self.clientes:
+            print(cliente.mostrar_informacion())
 
-            if cliente.id_cliente == id_cliente:
-                print(cliente)
-                return
-
-        print("Cliente no encontrado.")
+        print()
