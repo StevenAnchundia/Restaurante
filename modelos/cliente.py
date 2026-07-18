10abea5f824fdcd6acd2ca30068270d5ec9edbb3
Erick Anchundia <erick.anchundia01@cu.ucsg.edu.ec>
2026-07-18 08:29:07 -0500

    Enhance Cliente class with docstring and identificacion
    Added docstring and identificacion attribute to Cliente class.

--- modelos/cliente.py
+++ modelos/cliente.py
@@ -2,6 +2,17 @@ from dataclasses import dataclass
 
 @dataclass
 class Cliente:
-    id_cliente: int
+    """
+    Clase que representa un cliente.
+    """
+
+    identificacion: str
     nombre: str
     correo: str
+
+    def mostrar_informacion(self) -> str:
+        return (
+            f"Identificación: {self.identificacion} | "
+            f"Nombre: {self.nombre} | "
+            f"Correo: {self.correo}"
+        )
